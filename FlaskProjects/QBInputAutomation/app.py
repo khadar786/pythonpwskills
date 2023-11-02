029a716c023fc02f825f8d9771a6347b642f4367
khadar786 <khadarsayyad@gmail.com>
2023-11-02 19:16:52 +0530

    Pandas and Fastapi

--- FlaskProjects/QBInputAutomation/app.py
+++ FlaskProjects/QBInputAutomation/app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request,redirect,url_for,session
 import os,glob,json
 import pathlib
-from fastapi import Body, FastAPI,Request
+from fastapi import Body, FastAPI, Path, Query,Request
 from fastapi.middleware.wsgi import WSGIMiddleware
 #Ramesh sir
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr,Field
 import uvicorn
 from enum import Enum
 from typing import Optional
@@ -136,7 +136,154 @@ async def create_item_with_put(item_id,item:Item,q:str|None=None):
   return result
 
 #Query Parameters and String Validation
+@app.get("/qitems")
+async def read_items(q:str|None=Query(None,max_length=10)):
+  results={"items":[{"item_id":"Foo"},{"item_id":"Bar"}]}
+  if q:
+    results.update({
+      "q":q
+    })
+    
+  return results
 
+@app.get("/qitems1")
+#min and max length validation
+#async def read_items(q:str|None=Query(None,min_length=3,max_length=10,,regex="^fixedquery$")):
+#default value
+#async def read_items(q:str=Query("fixedquery",min_length=3,max_length=10)):
+#... is requried validation
+#async def read_items(q:str=Query(...,min_length=3,max_length=10))
+#pass multiple perameters
+#async def read_items(q:list[str]=Query(["foo","bar"])):
+#title,description,alias name
+async def read_items(q:str
+                     |None=Query(
+                       None,
+                       min_length=3,
+                       max_length=10,
+                       title="Sample query text",
+                       description="Sample query text",
+                       alias="item-query"
+                       )
+                     ):
+  results={"items":[{"item_id":"Foo"},{"item_id":"Bar"}]}
+  if q:
+    results.update({
+      "q":q
+    })
+    
+  return results
+
+@app.get("/items_hidden")
+def hidden_query_route(hidden_query:str|None=Query(None,include_in_schema=False)):
+  if hidden_query:
+    return {"hidden_query":hidden_query}
+  
+
+#Path Parameters and Numeric Validation
+# @app.get("/items_validation/{item_id}")
+# async def read_items_validation(
+#   *,
+#   item_id:int=Path(...,title="The ID of the item to get",ge=10,le=100),
+#   q:str='hello'
+# ):
+  
+@app.get("/items_validation/{item_id}")
+async def read_items_validation(
+  item_id:int=Path(...,title="The ID of the item to get",gt=10,lt=100),
+  q:str='hello',
+  size:float=Query(...,gt=0,lt=7.75)
+):
+  results={"item_id":item_id}
+  if q:
+    results.update({"q":q,"size":size})
+    
+  return results
+
+"""
+  Body Multiple Parameters
+"""  
+class NewItem(BaseModel): 
+  name:str
+  description:str|None=None
+  price:float
+  tax:float|None=None
+
+class User(BaseModel):
+  username:str
+  full_name:str|None=None
+  
+  
+@app.put("/newitems/{item_id}")
+async def udate_item(
+  *,
+  item_id:int=Path(...,title="The ID of the itme to get",ge=0,le=150),
+  q:str | None=None,
+  item:NewItem|None=None,
+  user:User,
+  importance:int=Body(...)
+):
+  results={"item_id":item_id}
+  
+  if q:
+    results.update({"q":q})
+  
+  if item:
+    results.update({"item":item})
+  
+  if user:
+    results.update({"user":user})
+  
+  if importance:
+    results.update({"importance":importance})
+    
+  return results
+
+@app.put("/newitems2/{item_id}")
+async def udate_item(
+  *,
+  item_id:int=Path(...,title="The ID of the itme to get",ge=0,le=150),
+  q:str | None=None,
+  item:NewItem=Body(...,embed=True),
+):
+  results={"item_id":item_id}
+  
+  if q:
+    results.update({"q":q})
+  
+  if item:
+    results.update({"item":item})
+    
+  return results
+
+"""
+  Body Field
+"""
+class BItem(BaseModel):
+  name:str
+  description:str|None=Field(...,title="The description of the item",max_length=100)
+  price:float=Field(...,gt=0,description="The price must be greater than zero.")
+  tax:float|None=None
+
+@app.put("/bodyitems/{item_id}")
+async def update_item(
+  item_id:int,
+  item:BItem=Body(...,embed=True)
+):
+  result={"item_id":item_id,"item":item}
+  return result
+
+"""
+  Body - Nested Models
+"""
+class NSMItem(BaseModel):
+  name:str
+  description:str|None=None
+  price:float
+  tax:float|None=None
+  tags:list=[]
+
+#@app.put("/nsmitems/{item_id}")
 
 
 #Flask section
