937d0a5dc0ce053c2156cbf077d4dc7d92280576
khadar786 <khadarsayyad@gmail.com>
2023-11-03 19:26:34 +0530

    Pandas adv and fastapi

--- FlaskProjects/QBInputAutomation/app.py
+++ FlaskProjects/QBInputAutomation/app.py
@@ -1,10 +1,13 @@
+from datetime import datetime,time,timedelta
+import re
+from uuid import UUID
 from flask import Flask,render_template,request,redirect,url_for,session
 import os,glob,json
 import pathlib
-from fastapi import Body, FastAPI, Path, Query,Request
+from fastapi import Body, FastAPI, Path, Query,Request,Cookie,Form,File,Header
 from fastapi.middleware.wsgi import WSGIMiddleware
 #Ramesh sir
-from pydantic import BaseModel, EmailStr,Field
+from pydantic import BaseModel, EmailStr,Field, HttpUrl
 import uvicorn
 from enum import Enum
 from typing import Optional
@@ -276,14 +279,147 @@ async def update_item(
 """
   Body - Nested Models
 """
+class Image(BaseModel):
+  name:str
+  url:HttpUrl
+  
 class NSMItem(BaseModel):
   name:str
   description:str|None=None
   price:float
   tax:float|None=None
-  tags:list=[]
+  #tags:list[int]=[]
+  tags:set[str]=[]
+  #image:Image | None=None
+  image:list[Image] | None=None
 
-#@app.put("/nsmitems/{item_id}")
+class Offer(BaseModel):
+  name:str
+  description:str|None=None
+  price:float
+  items:list[NSMItem]
+
+@app.put("/nsmitems/{item_id}")
+async def nsmupdate_item(item_id:int,item:NSMItem):
+  results={"item_id":item_id,"item":item}
+  return results 
+
+@app.post("/offers")
+async def create_offer(offer:Offer=Body(...,embed=True)):
+  return offer
+
+@app.post("/images/multiple")
+async def create_multiple_images(images:list[Image]):
+  return images
+
+@app.post("/blah")
+async def create_some_blahs(blahs:dict[int,float]):
+  return blahs
+
+"""
+  Declare Request Example Data
+"""
+#Method 1
+class DRItem(BaseModel):
+  name:str
+  description:str|None=None
+  price:float
+  tax:float|None=None
+  
+  class Config:
+    json_schema_extra={
+      "example":{
+        "name":"Foo",
+        "description":"A very nice Item",
+        "price":16.25,
+        "tax":1.67
+      }
+    }
+
+class DRItem2(BaseModel):
+  name:str=Field(...,example="Foo")
+  description:str|None=Field(...,example="A very nice Item")
+  price:float=Field(...,example=16.25)
+  tax:float|None=Field(...,example=1.67)
+
+class DRItem3(BaseModel):
+  name:str
+  description:str|None=None
+  price:float|None=None
+  tax:float|None=None
+
+#@app.put("/dritems/{item_id}")
+# async def dr_update_items(item_id:int,item:DRItem3):
+#   results={"item_id":item_id,"item":item}
+#   return results
+
+# async def dr_update_items(item_id:int,item:DRItem3=Body(...,
+#                                                         example={
+#                                                           "name":"Foo",
+#                                                           "description":"A very nice Item",
+#                                                           "price":16.25,
+#                                                           "tax":1.67
+#                                                         }
+#                                                         )):
+#   results={"item_id":item_id,"item":item}
+#   return results
+@app.put("/dritems/{item_id}")
+async def dr_update_items(item_id:int,
+                          item:DRItem3=Body(...,
+                                            openapi_examples={
+                                              "normal": {
+                                                  "summary": "A normal example",
+                                                  "description": "A __normal__ item works _correctly_",
+                                                  "value": {
+                                                      "name": "Foo",
+                                                      "description": "A very nice Item",
+                                                      "price": 16.25,
+                                                      "tax": 1.67,
+                                                  },
+                                              },
+                                              "converted": {
+                                                  "summary": "An example with converted data",
+                                                  "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                                                  "value": {"name": "Bar", "price": "16.25"},
+                                              },
+                                              "invalid": {
+                                                  "summary": "Invalid data is rejected with an error",
+                                                  "description": "Hello youtubers",
+                                                  "value": {"name": "Baz", "price": "sixteen point two five"},
+                                              },
+                                            }
+                                          ),
+                          ):
+  results={"item_id":item_id,"item":item}
+  return results
+
+"""
+  Extra Data Types
+"""
+#288340a0-0f0b-49ed-b48d-8a9f6e1a2274
+@app.put("/edt_items/{item_id}")
+async def read_items(
+  item_id:UUID,
+  start_date:datetime | None=Body(None),
+  end_date:datetime | None=Body(None),
+  repeat_at:time | None=Body(None),
+  process_after:timedelta | None=Body(None)
+):
+ start_process=start_date+process_after
+ duration=end_date-start_process
+ return {
+   "item_id":item_id,
+   "start_date":start_date,
+   "end_date":end_date,
+   "repeat_at":repeat_at,
+   "process_after":process_after,
+   "start_process":start_process,
+   "duration":duration
+ }
+ 
+ """
+  Cookie and Header Parameters
+"""
 
 
 #Flask section
